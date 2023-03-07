39926b07913b8a1f79b3352a34013ccd38d772dc
SVA2021 <sofronov.vit@gmail.com>
2023-03-07 19:24:10 +0400

    add initial types

---
+++ src/types.ts
@@ -0,0 +1,11 @@
+export const DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ',',] as const;
+export type digitT = typeof DIGITS[number];
+
+export const OPERATORS = ['/', 'x', '-', '+',] as const;
+export type operatorT = typeof OPERATORS[number];
+
+export const MODE_TYPES = ['runtime' , 'constructor'];
+export type modeT = typeof MODE_TYPES[number];
+
+export const CALCULATOR_PARTS = ['screen', 'operators', 'digits', 'equal',] as const;
+export type calculatorPartT = typeof CALCULATOR_PARTS[number];
