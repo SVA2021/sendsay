0c19be2d294fcc82d1a933573dc80ebeb4aa3375
SVA2021 <sofronov.vit@gmail.com>
2023-03-08 14:08:46 +0400

    base layout changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import {Calculator} from './components';
 
 function App(): JSX.Element {
   return (
     <div className="App">
-      
+      <Calculator />
     </div>
   );
 }
