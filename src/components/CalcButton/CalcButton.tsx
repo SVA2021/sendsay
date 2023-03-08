e9778f69ac8da8f370e8839584f8c3f293e15dd6
SVA2021 <sofronov.vit@gmail.com>
2023-03-08 14:10:14 +0400

    upd functions type add gridarea

--- src/components/CalcButton/CalcButton.tsx
+++ src/components/CalcButton/CalcButton.tsx
@@ -3,13 +3,14 @@ import {digitT, operatorT} from '../../types';
 import s from './CalcButton.module.scss';
 
 export type CalcButtonPropsT = {
-  clickHandler: (v: any) => void
+  clickHandler?: (v: any) => void
   value: digitT | operatorT
+  style?: string
 }
 
-export const CalcButton: FC<CalcButtonPropsT> = ({clickHandler, value}) => {
+export const CalcButton: FC<CalcButtonPropsT> = ({clickHandler, value, style}) => {
   return (
-    <div onClick={clickHandler} className={s.calcButton} >
+    <div onClick={clickHandler} className={s.calcButton} style={{gridArea: style}}>
       {value}
     </div>
   );
