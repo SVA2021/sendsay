593c465938601c8866f1b938162e22bc189471cb
SVA2021 <sofronov.vit@gmail.com>
2023-03-08 14:09:00 +0400

    fix bug with digits

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,10 @@
-export const DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ',',] as const;
+export const DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ',',] as const;
 export type digitT = typeof DIGITS[number];
 
 export const OPERATORS = ['/', 'x', '-', '+',] as const;
 export type operatorT = typeof OPERATORS[number];
 
-export const MODE_TYPES = ['runtime' , 'constructor'];
+export const MODE_TYPES = ['runtime', 'constructor'] as const;
 export type modeT = typeof MODE_TYPES[number];
 
 export const CALCULATOR_PARTS = ['screen', 'operators', 'digits', 'equal',] as const;
