e5093ffb31807d397879d9b120af21fff559dfe9
SVA2021 <sofronov.vit@gmail.com>
2023-03-09 16:30:52 +0400

    upd mode toggle

--- src/components/ModeToggler/ModeToggler.tsx
+++ src/components/ModeToggler/ModeToggler.tsx
@@ -4,19 +4,22 @@ import s from './ModeToggler.module.scss';
 import {ReactComponent as EyeIcon} from '../../assets/eye.svg';
 import {ReactComponent as SelectorIcon} from '../../assets/selector.svg';
 import classNames from 'classnames';
+import {useAppDispatch, useAppSelector} from '../../app/hooks';
+import {selectMode, setMode} from '../../features/calculator/calculatorSlice';
 
-export type ModeTogglerProps = {
-  mode: modeT
-  clickHandler: (mode: modeT) => void
-}
+export const ModeToggler: FC = () => {
 
-export const ModeToggler: FC<ModeTogglerProps> = ({mode, clickHandler}) => {
+  const mode = useAppSelector(selectMode);
+  const dispatch = useAppDispatch();
+
+  const clickHandler = (newMode: modeT) => dispatch(setMode(newMode));
+
   return (
     <div className={s.toggler} >
       {MODE_TYPES.map((item) => {
         return (
           <div key={item}
-            className={classNames(s.toggler__item, mode === item ? s.toggler__item__active : '')}
+            className={classNames(s.toggler__item, mode === item && s.toggler__item__active)}
             onClick={() => clickHandler(item)}
           >
             {item === 'runtime' ? <EyeIcon /> : <SelectorIcon />}
