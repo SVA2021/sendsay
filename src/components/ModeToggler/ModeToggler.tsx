d88b01b24a9e5b0cb693d1198b0da64df08f68c9
SVA2021 <sofronov.vit@gmail.com>
2023-03-10 16:30:33 +0400

    upd change props for mode toggler

--- src/components/ModeToggler/ModeToggler.tsx
+++ src/components/ModeToggler/ModeToggler.tsx
@@ -1,33 +1,34 @@
-import React, {FC} from 'react';
-import {modeT, MODE_TYPES} from '../../types';
-import s from './ModeToggler.module.scss';
+import classNames from 'classnames';
+import {FC} from 'react';
 import {ReactComponent as EyeIcon} from '../../assets/eye.svg';
 import {ReactComponent as SelectorIcon} from '../../assets/selector.svg';
-import classNames from 'classnames';
-import {useAppDispatch, useAppSelector} from '../../app/hooks';
-import {selectMode, setMode} from '../../features/calculator/calculatorSlice';
+import {modeT, MODE_TYPES} from '../../types';
+import s from './ModeToggler.module.scss';
 
-export const ModeToggler: FC = () => {
+export type ModeTogglerPropsT = {
+  mode: modeT
+  changeMode: (newMode: modeT) => void
+}
 
-  const mode = useAppSelector(selectMode);
-  const dispatch = useAppDispatch();
-
-  const clickHandler = (newMode: modeT) => dispatch(setMode(newMode));
-
+export const ModeToggler: FC<ModeTogglerPropsT> = ({mode, changeMode}) => {
   return (
     <div className={s.toggler} >
-      {MODE_TYPES.map((item) => {
-        return (
-          <div key={item}
-            className={classNames(s.toggler__item, mode === item && s.toggler__item__active)}
-            onClick={() => clickHandler(item)}
-          >
-            {item === 'runtime' ? <EyeIcon /> : <SelectorIcon />}
-            <span className={s.toggler__text}>{item}</span>
-          </div>
-        )
+      {MODE_TYPES.map((item) =>
+        <div key={item}
+          className={
+            classNames(
+              s.toggler__item,
+              mode === item && s.toggler__item__active,
+            )}
+          onClick={() => changeMode(item)}
+        >
+          {item === 'runtime' ? <EyeIcon /> : <SelectorIcon />}
+          <span className={s.toggler__text}>
+            {item}
+          </span>
+        </div>
+      )
       }
-      )}
     </div>
   );
 };
