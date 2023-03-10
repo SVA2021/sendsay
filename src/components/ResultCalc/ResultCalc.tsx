2b5017e92c0168c367de6eb7244762c13d73c13f
SVA2021 <sofronov.vit@gmail.com>
2023-03-10 21:55:24 +0400

    upd result calc wrapper

--- src/components/ResultCalc/ResultCalc.tsx
+++ src/components/ResultCalc/ResultCalc.tsx
@@ -1,29 +1,18 @@
 import {FC} from 'react';
-import {useDrop} from 'react-dnd';
-import {EditCalc, Placeholder, WorkCalc} from '..';
+import {EditCalc, WorkCalc} from '..';
 import {useAppSelector} from '../../app/hooks';
-import {selectMode, selectResultConstrustor} from '../../features/constructCalc/constructCalcSlice';
-import {DnDItemTypes} from '../../types';
+import {selectMode} from '../../features/constructCalc/constructCalcSlice';
 
 export const ResultCalc: FC = () => {
 
   const mode = useAppSelector(selectMode);
-  const resultConstructor = useAppSelector(selectResultConstrustor);
-
-  const [{isOver}, drop] = useDrop(() => ({
-    accept: [DnDItemTypes.initial, DnDItemTypes.result],
-    collect: (monitor) => ({isOver: !!monitor.isOver(), }),
-  }))
-
   return (
-    <div ref={drop} >
+    <>
       {
-        resultConstructor.length > 0
-          ? mode === 'runtime'
-            ? <WorkCalc constructResult={resultConstructor} />
-            : <EditCalc constructResult={resultConstructor} />
-          : <Placeholder isOver={isOver} />
+        mode === 'runtime'
+          ? <WorkCalc />
+          : <EditCalc />
       }
-    </div>
+    </>
   );
 };
