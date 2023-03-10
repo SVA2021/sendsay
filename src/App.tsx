2318e13073915fb1a4fd48614d0d87f808ac035a
SVA2021 <sofronov.vit@gmail.com>
2023-03-10 17:54:37 +0400

    add Dnd wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import {Calculator} from './components';
+import {DndProvider} from 'react-dnd';
+import {HTML5Backend} from 'react-dnd-html5-backend';
 
 function App(): JSX.Element {
   return (
     <div className="App">
-      <Calculator />
+      <DndProvider backend={HTML5Backend}>
+        <Calculator />
+      </DndProvider>
     </div>
   );
 }
