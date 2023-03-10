9347957f581b6f42d60872870f573b5805ea7f91
SVA2021 <sofronov.vit@gmail.com>
2023-03-10 17:52:30 +0400

    upd main calculator divide to components

--- src/components/Calculator/Calculator.tsx
+++ src/components/Calculator/Calculator.tsx
@@ -1,13 +1,7 @@
-import {InitialCalc, ModeToggler, Placeholder, ResultCalc} from '..';
-import {useAppSelector} from '../../app/hooks';
-import {selectMode, selectResultConstrustor} from '../../features/calculator/calculatorSlice';
+import {InitialCalc, ModeToggler, ResultCalc} from '..';
 import s from './Calculator.module.scss';
 
 export const Calculator = () => {
-
-  const mode = useAppSelector(selectMode);
-  const resultConstructor = useAppSelector(selectResultConstrustor);
-
   return (
     <div className={s.layout}>
       <div className={s.empty}></div>
@@ -15,14 +9,10 @@ export const Calculator = () => {
         <ModeToggler />
       </div>
       <div className={s.initial}>
-        {mode === 'constructor' && <InitialCalc />}
+        <InitialCalc />
       </div>
       <div className={s.result} >
-        {
-          resultConstructor.length > 0
-            ? <ResultCalc />
-            : <Placeholder />
-        }
+        <ResultCalc />
       </div>
     </div>
   );
