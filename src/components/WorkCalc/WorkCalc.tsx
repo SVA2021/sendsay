075b46417cc453ffd54807424dc4d14691c66d2b
SVA2021 <sofronov.vit@gmail.com>
2023-03-10 21:56:21 +0400

    upd props

--- src/components/WorkCalc/WorkCalc.tsx
+++ src/components/WorkCalc/WorkCalc.tsx
@@ -1,19 +1,18 @@
 import {FC} from 'react';
-import {DigitsBlock, Equals, OperatorsBlock, Screen} from '..';
+import {DigitsBlock, Equals, OperatorsBlock, Placeholder, Screen} from '..';
 import {useAppDispatch, useAppSelector} from '../../app/hooks';
-import {setSecondValue, setFirstValue, setOperator, calculate, selectOperator, selectScreen} from '../../features/calculator/calculatorSlice';
-import {calculatorPartT, digitT, operatorT} from '../../types';
+import {calculate, selectOperator, selectScreen, setFirstValue, setOperator, setSecondValue} from '../../features/calculator/calculatorSlice';
+import {selectResultConstrustor} from '../../features/constructCalc/constructCalcSlice';
+import {digitT, operatorT} from '../../types';
 import s from './WorkCalc.module.scss';
 
-export type WorCalcPropsT = {
-  constructResult: calculatorPartT[]
-}
-
-export const WorkCalc: FC<WorCalcPropsT> = ({constructResult}) => {
+export const WorkCalc: FC = () => {
 
   const dispatch = useAppDispatch();
   const operator = useAppSelector(selectOperator);
   const screenValue = useAppSelector(selectScreen);
+  const resultConstructor = useAppSelector(selectResultConstrustor);
+
   const addDigit = (v: digitT) => operator ? dispatch(setSecondValue(v)) : dispatch(setFirstValue(v));
   const addOperator = (v: operatorT) => dispatch(setOperator(v));
   const calculateToScreen = () => dispatch(calculate());
@@ -21,14 +20,16 @@ export const WorkCalc: FC<WorCalcPropsT> = ({constructResult}) => {
   return (
     <>
       {
-        constructResult.map((item) =>
-          <div key={item} className={s.work__item}>
-            {item === 'screen' && <Screen value={screenValue} />}
-            {item === 'digits' && <DigitsBlock clickHandler={addDigit} />}
-            {item === 'operators' && <OperatorsBlock clickHandler={addOperator} />}
-            {item === 'equal' && <Equals clickHandler={calculateToScreen} />}
-          </div>
-        )
+        resultConstructor.length > 0
+          ? resultConstructor.map((item) =>
+            <div key={item} className={s.work__item}>
+              {item === 'screen' && <Screen value={screenValue} />}
+              {item === 'digits' && <DigitsBlock clickHandler={addDigit} />}
+              {item === 'operators' && <OperatorsBlock clickHandler={addOperator} />}
+              {item === 'equal' && <Equals clickHandler={calculateToScreen} />}
+            </div>
+          )
+          : <Placeholder />
       }
     </>
   );
