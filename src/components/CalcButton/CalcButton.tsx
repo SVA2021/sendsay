fc8bb126527d8c8cc79b95383996fc75b8c8c319
SVA2021 <sofronov.vit@gmail.com>
2023-03-10 14:47:09 +0400

    upd props parameters

--- src/components/CalcButton/CalcButton.tsx
+++ src/components/CalcButton/CalcButton.tsx
@@ -5,12 +5,18 @@ import s from './CalcButton.module.scss';
 export type CalcButtonPropsT = {
   clickHandler?: (v: any) => void
   value: digitT | operatorT
-  style?: string
+  gridAreaName?: string
 }
 
-export const CalcButton: FC<CalcButtonPropsT> = ({clickHandler, value, style}) => {
+export const CalcButton: FC<CalcButtonPropsT> = ({clickHandler, value, gridAreaName}) => {
+
+  const handleClick = (value: digitT | operatorT) => {
+    if (!clickHandler) return;
+    clickHandler(value);
+  }
+
   return (
-    <div onClick={clickHandler} className={s.calcButton} style={{gridArea: style}}>
+    <div onClick={() => handleClick(value)} className={s.calcButton} style={{gridArea: gridAreaName}}>
       {value}
     </div>
   );
