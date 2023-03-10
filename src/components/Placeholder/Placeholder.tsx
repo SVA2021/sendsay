e5ec810e2c67a29565586344c6bc777cef07ebf9
SVA2021 <sofronov.vit@gmail.com>
2023-03-10 17:45:36 +0400

    upd props

--- src/components/Placeholder/Placeholder.tsx
+++ src/components/Placeholder/Placeholder.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, {FC} from 'react';
 import s from './Placeholder.module.scss'
 import {ReactComponent as AddIcon} from '../../assets/add.svg';
+import classNames from 'classnames';
 
-export const Placeholder = () => {
+export type PlaceholderProps = {
+  isOver: boolean
+}
+
+export const Placeholder: FC<PlaceholderProps> = ({isOver}) => {
   return (
-    <div className={s.placeholder} >
+    <div className={classNames(s.placeholder, isOver && s.placeholder__over)} >
       <p className={s.placeholder__icon}><AddIcon /></p>
       <p className={s.placeholder__title}>Перетащите сюда</p>
       <p className={s.placeholder__text}>любой элемент<br /> из левой панели</p>
